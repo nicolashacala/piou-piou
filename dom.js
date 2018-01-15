bb408af2b5dd99a9533e5ac3f333575dd6594145
nicolashacala <nicolas.hacala@gmail.com>
2018-01-15 16:50:06 +0100

    eod 15/01

---
+++ dom.js
@@ -0,0 +1,7 @@
+console.log(scoreFinal);
+
+var scoreTable = document.createElement('DIV');
+scoreTable.className = 'score_table';
+document.body.insertAdjacentElement('afterbegin', scoreTable);
+
+import scoreFinal from 'canvas.js';
--- game.js
+++ game.js
@@ -13,17 +13,20 @@ var ballTraject = [];
 var targetX = Math.floor(Math.random()*451);
 var targetY = Math.floor(Math.random()* 201);
 var randomNumber = Math.floor(Math.random()*2);
+var secToStart;
 var min = 0;
-var sec = 4;
+var sec = 5;
 var score = 0;
+var scoreFinal = [];
+var highestScores = [];
 var targetAlex = new Image();
 targetAlex.src = '393415.jpeg';
 var targetEmily = new Image();
 targetEmily.src = '18309559.png';
 var malus = new Image();
 malus.src = 'malus.jpeg';
-var malusX = Math.floor(Math.random()*451);
-var malusY = Math.floor(Math.random()* 201);
+var malusX;
+var malusY;
 var explosion = new Image();
 var explosionX = [];
 var explosionY = [];
@@ -32,6 +35,7 @@ explosion.src = 'explosion.png';
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
+
 
 
 function mouseMoveHandler(e) {
@@ -127,37 +131,15 @@ function draw(){
 	if(launchBall && ballY == canvas.height-canonHeight+10){
 		ballTraject[0] = canonX;
 	}
-	
-	
-	if(launchBall && ballY > 0 && score >= 4000){
-		ballY -= 35;
-	}
-	else if(launchBall && ballY > 0 && score >= 3500){
-		ballY -= 30;
-	}
-	else if(launchBall && ballY > 0 && score >= 3000){
-		ballY -= 25;
-	}
-	else if(launchBall && ballY > 0 && score >= 2000){
-		ballY -= 20;
-	}
-	else if(launchBall && ballY > 0 && score >= 1000){
-		ballY -= 15;
-	}
-	else if(launchBall && ballY > 0){
-		ballY -= 10;
-		ballX = ballTraject[0];
-	}
-	else{
-		launchBall = false;
-		ballY = canvas.height-canonHeight+10;
-		ballX = canonX;
 
+	if(score >= 6000){
+		ballR = 25;
 	}
-
-	
-	if(score >= 4000){
-		ballR = 2;
+	else if(score >= 5000){
+		ballR = 15;
+	}
+	else if(score >= 4000){
+		ballR = 10;
 	}
 	else if(score >= 3500){
 		ballR = 5;
@@ -171,8 +153,42 @@ function draw(){
 	else if(score >= 1000){
 		ballR = 20;
 	}
+	
+	if(launchBall && ballY > 0){
+		if(score >= 6000){
+			ballY -= 15;
+		}
+		else if(score >= 5000){
+			ballY -= 19;
+		}
+		else if(score >= 4000){
+			ballY -= 30;
+		}
+		else if(score >= 3500){
+			ballY -= 24;
+		}
+		else if(score >= 3000){
+			ballY -= 19;
+		}
+		else if(score >= 2000){
+			ballY -= 15;
+		}
+		else if(score >= 1000){
+			ballY -= 12;
+		}
+		ballY -= 10;
+		ballX = ballTraject[0];
+	}
+	else{
+		launchBall = false;
+		ballY = canvas.height-canonHeight+10;
+		ballX = canonX;
 
+	}
+	
 	requestAnimationFrame(draw);
+		
+	
 }
 
 function collisionDetection(){
@@ -227,34 +243,142 @@ function collisionDetection(){
 		launchBall = false;
 		drawTarget();
 	}
+	else if(malusX < ballX+ballR && malusX+60 > ballX-ballR && malusY+60 > ballY+ballR){
+		if(score >= 4000){
+			sec -= 10
+		}
+		else{
+			sec -= 5;
+		}
+		explosionX[0] = malusX;
+		explosionY[0] = malusY;
+		explosionTime = true;
+		
+		if(explosionTime && malusX == explosionX && malusY == explosionY){
+			setTimeout(() => {
+				explosionTime = false;
+			}, 1000);
+		}
+		malusX = Math.floor(Math.random()*451);
+		malusY = Math.floor(Math.random()* 201);
+		
+		ballX = canonX;
+		ballY = canvas.height-canonHeight+10;
+		launchBall = false;
+		drawMalus();
+	}
 }
 
 function drawCountDown(){
 	ctx.font = '16px Arial';
-	ctx.fillStyle = '#0095DD';
+	ctx.fillStyle = '#FFF';
 	if(sec<10){
 		ctx.fillText('Time left : ' + min + ':0' + sec, 380, 20);
 	}
 	else{
-	ctx.fillText('Time left: ' + min + ':' + sec, 380, 20);
+	ctx.fillText('Time left: ' + min + ':' + sec, 420, 20);
 	}
 }
 
 function drawScore(){
 	ctx.font = '16px Arial';
-	ctx.fillStyle = '#0095DD';
-	ctx.fillText('Score: ' + score, 10, 20);
+	ctx.fillStyle = '#FFF';
+	ctx.fillText('Score: ' + score, 40, 20);
 }
 
-window.onload = function(){
+function timer(){
 	setInterval(function(){
 		sec--;
 
-		if(sec == 0){
-			alert('You are a looser. Try again?');
-			document.location.reload();
+		if(sec <= 0){
+			scoreFinal.push(score);
+			alert('Final score: ' + score + '\nYou are a looser. Try again?');
+			addHighestScore();
+			malusX = undefined;
+			malusY = undefined;
+			ballX = canonX;
+			ballY = canvas.height-canonHeight+10;
+			sec = 5;
+			score = 0;
+			ballR = 30;
+			targetX = Math.floor(Math.random()*451);
+			targetY = Math.floor(Math.random()* 201);
 		}
 
 	}, 1000);
 }
-draw();
+
+function timerToStart(){
+	secToStart = 5;
+	setInterval(function(){
+		startingScreen.style.display = 'none';
+		secToStart--;
+		if(secToStart >= 3){
+			ctx.font = 'bold 20px Arial';
+			ctx.fillStyle = '#cc0000';
+			ctx.textAlign = "center";
+			ctx.fillText('READY?', canvas.width/2, 200);
+		}
+		else if(secToStart == 1){
+			ctx.clearRect(0,0, canvas.width, canvas.height);
+			ctx.font = 'bold 20px Arial';
+			ctx.fillStyle = '#cc0000';
+			ctx.textAlign = "center";
+			ctx.fillText('GO', canvas.width/2, 200);
+		}
+		else if(secToStart == 0){
+			timer();
+			draw();
+		}
+	}, 1000);
+}
+
+
+
+
+// Dom
+
+var startGame = document.createElement('BUTTON');
+startGame.className = 'start_game_button';
+startGame.appendChild(document.createTextNode('Start'));
+startGame.addEventListener('click', timerToStart);
+
+var gameRules = document.createElement('DIV');
+gameRules.className = 'game_rules';
+gameRules.innerHTML = 'Use the mouse to move the canon <br/>Use the space bar to shoot.';
+
+var startingScreen = document.createElement('DIV');
+startingScreen.className = 'starting_screen';
+startingScreen.appendChild(gameRules);
+startingScreen.appendChild(startGame);
+document.body.appendChild(startingScreen);
+
+
+var scoreTable = document.createElement('DIV');
+scoreTable.className = 'score_table';
+document.body.insertAdjacentElement('afterbegin', scoreTable);
+
+var titleScoreHigh = document.createElement('H3');
+titleScoreHigh.appendChild(document.createTextNode('Highest scores:'));
+scoreTable.appendChild(titleScoreHigh);
+
+var highestScoreList = document.createElement('UL');
+scoreTable.appendChild(highestScoreList);
+
+
+function addHighestScore(){
+
+	highestScores = scoreFinal.sort(function(a, b){return b-a});
+
+	while(highestScoreList.firstChild){
+		highestScoreList.removeChild(highestScoreList.firstChild);
+	}
+
+	highestScores.forEach(function(scoreSorted){
+		var highestScoreListElement = document.createElement('LI');
+		highestScoreListElement.appendChild(document.createTextNode(scoreSorted));
+		highestScoreList.appendChild(highestScoreListElement);
+	})
+
+}
+
